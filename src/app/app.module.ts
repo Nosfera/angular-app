63b709b250f56a714bca05408739b493b9831ac6
Juan Zerbini Berro <juanze82@hotmail.com>
2017-12-03 15:40:37 +0100

    First steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
+// Ng decorator linked to the appModule class
+/*
+ a function that modifies angular clases
+ takes meta-data to help you describe the modules
 
+*/
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+MaterialModule,
+FlexLayoutModule
   ],
+  // the providers defines all the services that this module will be using
   providers: [],
   bootstrap: [AppComponent]
 })
+
+// This exports the main class or the bootstrapped modules into
+// root AppModule
 export class AppModule { }
