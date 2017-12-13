3832ac78de3fb496a9f1a1398fdd7fb500ea5309
Juan Zerbini Berro <juanze82@hotmail.com>
2017-12-13 23:43:23 +0100

    App with new components: Header, contact, routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
